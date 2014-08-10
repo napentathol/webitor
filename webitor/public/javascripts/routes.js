4a273c3ca3400faf4dab15dabcc0a3a4d3b9d541
Alex <ac.webster1990@gmail.com>
2014-08-10 01:38:53 -0500

    initial commit part two.

--- webitor/public/javascripts/routes.js
+++ webitor/public/javascripts/routes.js
@@ -1,3 +1,17 @@
 /**
  * Created by Alex on 8/9/2014.
  */
+
+var ideApp ={
+    $ng : angular.module('ideApp', [ 'ngRoute', 'ngAnimate', 'ui.ace' ])
+};
+
+ideApp.$ng.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/editor', {
+            templateUrl: 'editor'
+        }).otherwise({
+            redirectTo: 'editor'
+        });
+    }
+]);
